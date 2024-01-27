a60f156826cdb5ee27a47bec31365431c3d16e19
kharle14 <enriquezkharl14@gmail.com>
2024-01-27 21:58:12 +0800

    Get notes api route

---
+++ src/app/api/notes/route.ts
@@ -0,0 +1,48 @@
+import { getCurrentUser } from '@/lib/server/auth';
+import { sql } from '@/lib/server/db';
+
+export async function GET(req: Request) {
+  const { searchParams } = new URL(req.url);
+  const parent_id = searchParams.get('parent_id');
+
+  const user = await getCurrentUser();
+
+  let noteRes = null;
+
+  if (parent_id) {
+    noteRes = await sql(
+      'select * from notes where user_id = $1 and parent_id = $2 order by title asc',
+      [user?.id, parent_id]
+    );
+  } else {
+    noteRes = await sql(
+      'select * from notes where user_id = $1 and parent_id is null order by title asc',
+      [user?.id]
+    );
+  }
+
+  const ids = noteRes.rows.map((row) => row.id);
+
+  const childNotesCount = await sql(
+    'select parent_id, count(*)::int from notes where parent_id = any($1) group by parent_id',
+    [ids]
+  );
+
+  const childNoteCountMap = childNotesCount.rows.reduce((map, row) => {
+    map[row.parent_id] = row.count;
+
+    return map;
+  }, {});
+
+  console.log(childNoteCountMap);
+
+  noteRes.rows.forEach((row) => {
+    if (childNoteCountMap.hasOwnProperty(row.id)) {
+      row.child_count = childNoteCountMap[row.id];
+    } else {
+      row.child_count = 0;
+    }
+  });
+
+  return Response.json({ notes: noteRes.rows });
+}
