b8a5381c187bb4d6f72a223308d00b2ad4433fde
kharle14 <enriquezkharl14@gmail.com>
2024-01-28 19:03:51 +0800

    Fetch notes API helper
    Added api helper for fetching notes
    Added luxon for easy handling of dates
    Update types of created/updated at to DateTime

---
+++ src/lib/client/api.ts
@@ -0,0 +1,29 @@
+import { NoteData } from '@/types/note';
+import { DateTime } from 'luxon';
+
+function transformJsonToNote(json: any): NoteData {
+  return {
+    ...json,
+    created_at: DateTime.fromISO(json.created_at),
+    updated_at: DateTime.fromISO(json.updated_at),
+    child_notes: [],
+  };
+}
+
+export async function fetchNotes(parent_id?: string) {
+  let queryString = '';
+
+  if (parent_id) {
+    queryString += '?parent_id=' + parent_id;
+  }
+
+  const notesRes = await fetch('/api/notes' + queryString);
+
+  const json = await notesRes.json();
+
+  console.log(json);
+
+  const transformed = json.map((data: any) => transformJsonToNote(data));
+
+  return transformed;
+}
--- src/types/note.ts
+++ src/types/note.ts
@@ -1,3 +1,5 @@
+import { DateTime } from 'luxon';
+
 export interface NoteData {
   id: string;
   user_id: string;
@@ -5,8 +7,8 @@ export interface NoteData {
   title: string;
   content: string;
   is_published: boolean;
-  created_At: Date;
-  update_At: Date;
+  created_at: DateTime;
+  updated_at: DateTime;
   child_notes: Array<NoteData>;
   child_count: number;
 }
